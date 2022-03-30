5e50621458f97f17da207f4c5a228f6586ca9a4a
jtanau <jean-marc.tanau@etudiant.univ-rennes1.fr>
2022-03-30 19:20:00 +0200

    Fix interaction.js

--- interaction.js
+++ interaction.js
@@ -3,8 +3,44 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.x = 0;
+  this.y = 0;
+  this.xFinal = 0;
+  this.yFinal = 0;
+  this.isClique = false;
 
 	// Developper les 3 fonctions gérant les événements
+  this.sourisCliquer = function (evt) {
+    const pos = getMousePosition(canvas, evt)
+    this.xFinal = this.x = pos.x
+    this.yFinal = this.y = pos.y
+    this.isClique = true
+    interactor.onInteractionStart(this)
+    console.log(evt)
+  }.bind(this);
+
+  this.sourisEnDeplacement = function (evt) {
+    if(this.isClique) {
+      this.x = this.xFinal
+      this.y = this.yFinal
+      const pos = getMousePosition(canvas, evt)
+      this.xFinal = pos.x
+      this.yFinal = pos.y
+      interactor.onInteractionUpdate(this)
+      console.log(evt)
+    }
+  }.bind(this);
+
+  this.sourisRelacher = function (evt) {
+    if(this.isClique) {
+      const pos = getMousePosition(canvas, evt)
+      this.xFinal = pos.x
+      this.yFinal = pos.y
+      this.isClique = false
+      interactor.onInteractionEnd(this)
+      console.log(evt)
+    }
+  }.bind(this);
 
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
